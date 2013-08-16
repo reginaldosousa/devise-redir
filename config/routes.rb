e05728445b88a0c85bee3adcc2e8d223480df241
Reginaldo Sousa <reginaldo@corp.globo.com>
2013-08-16 18:31:16 -0300

    Alterando comportamento padrao do devise para redirecionar para a pagina requisitada apos o login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Devise::Application.routes.draw do
   resources :posts
 
   devise_for :users
+  
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
