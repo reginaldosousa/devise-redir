e6ba4517e5906c92aab39f142fdbe21f8e5018ba
Reginaldo Sousa <reginaldo@corp.globo.com>
2013-08-16 18:18:28 -0300

    Devie example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devise::Application.config.secret_key_base = '8a0ba44070802f6eaacb47d87c985e3bc88bd706399a881b80901087fcd1f72b160c73f6b0bacaf961b0a9c7896034a964163a237d6c75aff7034cbf2ce7f524'
